cde90dece86302f39200ed4bd8eed86e685ea571
Cameron Moreau <cameron.cm6@gmail.com>
2018-02-24 17:16:20 -0600

    added draggable list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,71 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`,
+  }));
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250,
+});
+
 class App extends Component {
+  state = {
+    commands: [
+      { id: 'forward', content: 'GO FORWARD' },
+      { id: 'back', content: 'GO BACK' },
+      { id: 'left', content: 'GO LEFT' },
+      { id: 'right', content: 'GO RIGHT' }
+    ],
+  };
+
+  onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const commands = reorder(
+      this.state.commands,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      commands,
+    });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +77,39 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        
+        <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.commands.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div>
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       </div>
     );
   }
